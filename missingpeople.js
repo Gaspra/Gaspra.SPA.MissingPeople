d51121a4ef03a27894229ffe908c1d1024154297
Richard Lawrence <35634732+Gaspra@users.noreply.github.com>
2024-04-25 14:13:59 +0100

    example assets

---
+++ missingpeople.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#Soundcloud").on('click', function() {
+        window.open('https://www.facebook.com/missingpeopleparty/', '_blank');
+    });
+
+    $("#Facebook").on('click', function() {
+        window.open('https://www.facebook.com/missingpeopleparty/', '_blank');
+    });
+
+    $("#Instagram").on('click', function() {
+        window.open('https://www.facebook.com/missingpeopleparty/', '_blank');
+    });
+
+    $("#Spotify").on('click', function() {
+        window.open('https://www.facebook.com/missingpeopleparty/', '_blank');
+    });
+})
