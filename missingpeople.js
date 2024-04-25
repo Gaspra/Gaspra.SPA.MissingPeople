c89c04cf1f0f6160a5623909ad5f4d312d135797
Richard Lawrence <35634732+Gaspra@users.noreply.github.com>
2024-04-25 19:12:14 +0100

    setting up form

--- missingpeople.js
+++ missingpeople.js
@@ -14,4 +14,30 @@ $(document).ready(function() {
     $("#Spotify").on('click', function() {
         window.open('https://www.facebook.com/missingpeopleparty/', '_blank');
     });
+    
+    const form = document.querySelector('form'); // Select the form element
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); // Prevent default form submission
+
+        const formData = new FormData(form);
+        const jsonData = Object.fromEntries(formData.entries()); // Convert FormData to a regular object
+
+        $.ajax({
+            url: 'https://comms.gaspra.co.uk/email?apiKey=1234',
+            type: 'POST',
+            data: JSON.stringify(jsonData),
+            contentType: 'application/json',
+            dataType: 'json', // Optional: Specify expected response format
+            success: function(data) {
+                console.log('Success:', data);
+                // Display a success message or handle the API response
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error('Error:', textStatus, errorThrown);
+                // Display an error message
+            }
+        });
+        
+    });
 })
